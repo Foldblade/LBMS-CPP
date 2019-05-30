5eb021dcfdfb56a4124013f3b4b324db536de446
Foldblade <foldblade@yeah.net>
2019-05-30 13:54:34 +0800

    trial

--- LBMS/search_book.cpp
+++ LBMS/search_book.cpp
@@ -1,6 +1,7 @@
 #include "search_book.h"
 #include "qstring.h"
 #include "search_book_result_no.h"
+#include "domysql.h"
 
 search_book::search_book(QWidget *parent)
 	: QWidget(parent)
@@ -17,17 +18,15 @@ void search_book_result_no::slot1() {
 	this->hide();
 }
 void search_book::slot2() {
-	
 	x = new search_book_result_no;
-	QString isbn;
-	QString book_name;
-	QString writer;
-	QString publishing_house;
-	isbn = ui.isbn->text();
-	book_name = ui.book_name->text();
-	writer = ui.writer->text();
-	publishing_house = ui.publishing_house->text();
+	QString isbn = ui.isbn->text();
+	QString book_name = ui.book_name->text();
+	QString writer = ui.writer->text();
+	QString publishing_house = ui.publishing_house->text();
 	//这里需要加入数据库；
+	/*So the left work are made by F.B.*/
+	Operate search_book_no;
+	search_book_no.searchBook(isbn.toStdString(), book_name.toStdString(), writer.toStdString(), publishing_house.toStdString());
 	x->show();
 	this->hide();
 }
--- LBMS/search_book_result_no.cpp
+++ LBMS/search_book_result_no.cpp
@@ -16,6 +16,7 @@ search_book_result_no::search_book_result_no(QWidget *parent)
 	//ui.tableWidget->verticalHeader()->setVisible(false); // 隐藏水平header
 	ui.tableWidget->setSelectionBehavior(QAbstractItemView::SelectItems);   // 单个选中
 	ui.tableWidget->setSelectionMode(QAbstractItemView::ExtendedSelection);  // 可以选中多个
+
 	QString content[5][5];
 	content[0][0] = { "1" }; content[0][1] = { "a" }; content[0][2] = { "b" }; content[0][3] = { "c" }; content[0][4] = { "d" };
 	content[1][0] = { "2" }; content[1][1] = { "a" }; content[1][2] = { "b" }; content[1][3] = { "c" }; content[1][4] = { "d" };
