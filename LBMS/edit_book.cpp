1379bb80a32bc36f76fc58acd0b32c74263079a6
bijinmengxiang <46309653+bijinmengxiang@users.noreply.github.com>
2019-06-02 22:23:09 +0800

    correct about return to search_book_man
    correct about return to search_book_man

--- LBMS/edit_book.cpp
+++ LBMS/edit_book.cpp
@@ -1,5 +1,5 @@
 #include "edit_book.h"
-#include "search_book_result_man.h"
+#include "search_book_man.h"
 edit_book::edit_book(QWidget *parent)
 	: QWidget(parent)
 {
@@ -16,7 +16,7 @@ void edit_book::deleting() {
 
 }//É¾³ý
 void edit_book::back() {
-	search_book_result_man* x = new search_book_result_man;
+	search_book_man* x = new search_book_man;
 	x->show();
 	this->hide();
 }//·µ»Ø
